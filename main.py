ca9992c7657a7d7ddbf2138bdf079568ce6a449f
Eric Ford <ericford@mac.com>
2023-06-13 22:02:59 -0400

    Added tests for failure modes for lookup and remove.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, HTTPException
 from typing import Optional
+
+import requests
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    raise HTTPException(
+            status_code=404, detail="Appropriate endpoints are lookup, remove, export."
+        )
 
 
 @app.get("/lookup/{vin}")
-async def lookup(vin: Optional[str]=None):
+async def lookup(vin: Optional[str] = None):
     """
     Look up VIN. Return json with following fields:
 
@@ -23,21 +27,34 @@ async def lookup(vin: Optional[str]=None):
 
     If VIN is invalid, return 404 error.
     """
-    if len(vin) != 17:
-        raise HTTPException(status_code=404, detail="Invalid VIN. VIN must be 17 characters.")
-    return {"message": f"Lookup {vin}"}
+    if vin and len(vin) != 17:
+        raise HTTPException(
+            status_code=404, detail="Invalid VIN. VIN must be 17 characters."
+        )
+    url = f"https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/{vin}?format=json"
+    # Note, only search public domain records. No real reason for that; I
+    # just didn't change it from the example on the API usage page.
+    # Also, only getting a single record on the first call because we just
+    # need the number of results.
+    r = requests.get(url=url)
+    print(r)
+    return {"msg": f"Lookup {vin}"}
 
 
-@app.get("/remove")
-async def remove():
+@app.get("/remove/{vin}")
+async def remove(vin: Optional[str] = None):
     """
     Remove VIN from cache. Return True on success. False on failure. Failure
     mode is VIN doesn't exist in cache.
     """
-    return {"message": "Remove"}
+    if vin and len(vin) != 17:
+        raise HTTPException(
+            status_code=404, detail="Invalid VIN. VIN must be 17 characters."
+        )
+    return {"msg": "Remove"}
 
 
 @app.get("/export")
 async def export():
     """Export entier cache as parquet file."""
-    return {"message": "Export"}
+    return {"msg": "Export"}
